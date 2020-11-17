764c2bfabd75c65f1d160877b431d8d18046275e
Vlad <vladislav5008zo@gmail.com>
2020-11-17 17:50:34 +0300

    second block ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,27 @@
+const promoSlide = document.querySelectorAll('.promo__slide'),
+      promoSlideTitle = document.querySelectorAll('.promo__slide-title'),
+      promoSlideText = document.querySelectorAll('.promo__slide-text'),
+      promoSlideButton = document.querySelectorAll('.btn_slide');
+
+      for (let i = 0; i < promoSlide.length; i++) {
+        promoSlide[i].addEventListener('click', function (e) {
+            if (!(promoSlide[i].classList.contains('promo__slide_active')))
+            {
+            for (let j = 0; j < newsItem.length; j++) {
+                promoSlide[j].classList.remove('promo__slide_active');
+                promoSlideTitle[j].classList.remove('promo__slide-title_active');
+                promoSlideButton[j].classList.remove('btn_slide_active');
+                promoSlideText[j].classList.remove('promo__slide-text_active');
+            }
+        } 
+        promoSlide[i].classList.add('promo__slide_active');
+        promoSlideTitle[i].classList.add('promo__slide-title_active');
+        promoSlideButton[i].classList.add('btn_slide_active');
+        promoSlideText[i].classList.add('promo__slide-text_active');
+        });
+    }
+
+
 const newsItem = document.querySelectorAll('.news__item');
 
 for (let i = 0; i < newsItem.length; i++) {
